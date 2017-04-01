243d0df6d4a6e21a7ab08d0bdc4709021234d957
knockcode <hejunjian2012@163.com>
2017-04-02 07:04:49 +0800

    add http`

---
+++ server/net/http/HttpContext.h
@@ -0,0 +1,70 @@
+#ifndef SERVER_NET_HTTP_HTTPCONTEXT_H
+#define SERVER_NET_HTTP_HTTPCONTEXT_H
+
+#include <server/base/copyable.h>
+
+#include <server/net/http/HttpRequest.h>
+
+namespace server
+{
+namespace net
+{
+
+class HttpContext : public server::copyable
+{
+ public:
+  enum HttpRequestParseState
+  {
+    kExpectRequestLine,
+    kExpectHeaders,
+    kExpectBody,
+    kGotAll,
+  };
+
+  HttpContext()
+    : state_(kExpectRequestLine)
+  {
+  }
+
+  // default copy-ctor, dtor and assignment are fine
+
+  bool expectRequestLine() const
+  { return state_ == kExpectRequestLine; }
+
+  bool expectHeaders() const
+  { return state_ == kExpectHeaders; }
+
+  bool expectBody() const
+  { return state_ == kExpectBody; }
+
+  bool gotAll() const
+  { return state_ == kGotAll; }
+
+  void receiveRequestLine()
+  { state_ = kExpectHeaders; }
+
+  void receiveHeaders()
+  { state_ = kGotAll; }  // FIXME
+
+  void reset()
+  {
+    state_ = kExpectRequestLine;
+    HttpRequest dummy;
+    request_.swap(dummy);
+  }
+
+  const HttpRequest& request() const
+  { return request_; }
+
+  HttpRequest& request()
+  { return request_; }
+
+ private:
+  HttpRequestParseState state_;
+  HttpRequest request_;
+};
+
+}
+}
+
+#endif  // SERVER_NET_HTTP_HTTPCONTEXT_H
---
+++ server/net/http/HttpServer.h
@@ -0,0 +1,61 @@
+#ifndef SERVER_NET_HTTP_HTTPSERVER_H
+#define SERVER_NET_HTTP_HTTPSERVER_H
+
+#include <server/net/TcpServer.h>
+#include <boost/noncopyable.hpp>
+
+namespace server
+{
+namespace net
+{
+
+class HttpRequest;
+class HttpResponse;
+
+/// A simple embeddable HTTP server designed for report status of a program.
+/// It is not a fully HTTP 1.1 compliant server, but provides minimum features
+/// that can communicate with HttpClient and Web browser.
+/// It is synchronous, just like Java Servlet.
+class HttpServer : boost::noncopyable
+{
+ public:
+  typedef boost::function<void (const HttpRequest&,
+                                HttpResponse*)> HttpCallback;
+
+  HttpServer(EventLoop* loop,
+             const InetAddress& listenAddr,
+             const string& name,
+             TcpServer::Option option = TcpServer::kNoReusePort);
+
+  ~HttpServer();  // force out-line dtor, for scoped_ptr members.
+
+  EventLoop* getLoop() const { return server_.getLoop(); }
+
+  /// Not thread safe, callback be registered before calling start().
+  void setHttpCallback(const HttpCallback& cb)
+  {
+    httpCallback_ = cb;
+  }
+
+  void setThreadNum(int numThreads)
+  {
+    server_.setThreadNum(numThreads);
+  }
+
+  void start();
+
+ private:
+  void onConnection(const TcpConnectionPtr& conn);
+  void onMessage(const TcpConnectionPtr& conn,
+                 Buffer* buf,
+                 Timestamp receiveTime);
+  void onRequest(const TcpConnectionPtr&, const HttpRequest&);
+
+  TcpServer server_;
+  HttpCallback httpCallback_;
+};
+
+}
+}
+
+#endif  // SERVER_NET_HTTP_HTTPSERVER_H

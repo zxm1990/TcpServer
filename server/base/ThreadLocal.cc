6e25b16717d1373d6819f1a65dcc42ada809bf0e
knockcode <hejunjian2012@163.com>
2017-04-10 07:20:54 +0800

    add thread local

---
+++ server/base/ThreadLocal.cc
@@ -0,0 +1,53 @@
+#ifndef SERVER_BASE_THREADLOCAL_H
+#define SERVER_BASE_THREADLOCAL_H
+
+#include <server/base/Mutex.h>  // MCHECK
+
+#include <boost/noncopyable.hpp>
+#include <pthread.h>
+
+namespace server
+{
+
+template<typename T>
+class ThreadLocal : boost::noncopyable
+{
+ public:
+  ThreadLocal()
+  {
+    pthread_key_create(&pkey_, &ThreadLocal::destructor);
+  }
+
+  ~ThreadLocal()
+  {
+    pthread_key_delete(pkey_);
+  }
+
+  T& value()
+  {
+    T* perThreadValue = static_cast<T*>(pthread_getspecific(pkey_));
+    if (!perThreadValue)
+    {
+      T* newObj = new T();
+      pthread_setspecific(pkey_, newObj);
+      perThreadValue = newObj;
+    }
+    return *perThreadValue;
+  }
+
+ private:
+
+  static void destructor(void *x)
+  {
+    T* obj = static_cast<T*>(x);
+    typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
+    T_must_be_complete_type dummy; (void) dummy;
+    delete obj;
+  }
+
+ private:
+  pthread_key_t pkey_;
+};
+
+}
+#endif  // SERVER_BASE_THREADLOCAL_H
---
+++ server/base/test/ThreadLocalSingleton_test.cc
@@ -0,0 +1,60 @@
+#include <server/base/ThreadLocalSingleton.h>
+#include <server/base/CurrentThread.h>
+#include <server/base/Thread.h>
+
+#include <boost/bind.hpp>
+#include <boost/noncopyable.hpp>
+#include <stdio.h>
+
+class Test : boost::noncopyable
+{
+ public:
+  Test()
+  {
+    printf("tid=%d, constructing %p\n", server::CurrentThread::tid(), this);
+  }
+
+  ~Test()
+  {
+    printf("tid=%d, destructing %p %s\n", server::CurrentThread::tid(), this, name_.c_str());
+  }
+
+  const server::string& name() const { return name_; }
+  void setName(const server::string& n) { name_ = n; }
+
+ private:
+  server::string name_;
+};
+
+void threadFunc(const char* changeTo)
+{
+  printf("tid=%d, %p name=%s\n",
+         server::CurrentThread::tid(),
+         &server::ThreadLocalSingleton<Test>::instance(),
+         server::ThreadLocalSingleton<Test>::instance().name().c_str());
+  server::ThreadLocalSingleton<Test>::instance().setName(changeTo);
+  printf("tid=%d, %p name=%s\n",
+         server::CurrentThread::tid(),
+         &server::ThreadLocalSingleton<Test>::instance(),
+         server::ThreadLocalSingleton<Test>::instance().name().c_str());
+
+  // no need to manually delete it
+  // server::ThreadLocalSingleton<Test>::destroy();
+}
+
+int main()
+{
+  server::ThreadLocalSingleton<Test>::instance().setName("main one");
+  server::Thread t1(boost::bind(threadFunc, "thread1"));
+  server::Thread t2(boost::bind(threadFunc, "thread2"));
+  t1.start();
+  t2.start();
+  t1.join();
+  printf("tid=%d, %p name=%s\n",
+         server::CurrentThread::tid(),
+         &server::ThreadLocalSingleton<Test>::instance(),
+         server::ThreadLocalSingleton<Test>::instance().name().c_str());
+  t2.join();
+
+  pthread_exit(0);
+}
